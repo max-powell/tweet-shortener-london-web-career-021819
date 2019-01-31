aaee8cf455e72cf027431813eb7cc8e836f092fe
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-31 16:03:38 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,39 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+
+def word_substituter(tweet)
+  words = tweet.split
+  words.each_with_index do |word, index|
+    if dictionary.include?(word.downcase)
+      words[index] = dictionary[word.downcase]
+    end
+  end
+  words.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each {|tweet| puts word_substituter(tweet)}
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  selective_tweet_shortener(tweet)[0...140]
+end
